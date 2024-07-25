7d502dcaaba88889ab188802bf78ef0a9dcbb6cc
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-07-25 16:06:22 +0200

    chore: initial project setup

---
+++ SpeedTestCore/Measurement.swift
@@ -0,0 +1,12 @@
+//
+//  Measurement.swift
+//  SpeedTestCore
+//
+//  Created by Antonio on 25/07/24.
+//
+
+import Foundation
+
+public struct Measurement {
+    
+}
---
+++ SpeedTestCoreTests/SpeedTestCoreTests.swift
@@ -0,0 +1,13 @@
+//
+//  SpeedTestCoreTests.swift
+//  SpeedTestCoreTests
+//
+//  Created by Antonio on 25/07/24.
+//
+
+import XCTest
+import SpeedTestCore
+
+final class SpeedTestCoreTests: XCTestCase {
+    
+}
